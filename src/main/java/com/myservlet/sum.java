ab62ed465a9404c30d24e336f71a1395cab4078e
Mitroha <30327809+Mitroha@users.noreply.github.com>
2017-07-30 22:36:25 +0300

    lalala

--- src/main/java/com/myservlet/sum.java
+++ src/main/java/com/myservlet/sum.java
@@ -13,38 +13,38 @@ import java.util.Map;
 /**
  * Created by alexey_mitrokhin on 18/07/17.
  */
-@WebServlet(name = "sumServlet", urlPatterns={"/"})
+@WebServlet(name = "sumServlet", urlPatterns = {"/"})
 public class sum extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String urlString = new String(request.getRequestURL().toString());
         //String urlString=request.getRequestURL().toString();
 
         response.setContentType("text/html");
-        PrintWriter out=response.getWriter();
+        PrintWriter out = response.getWriter();
 
         //out.println(urlString);
 
         //String paramName = "one";
         //String paramValue = request.getParameter(paramName);
 
-        java.util.Map<String, String[]> parMap= request.getParameterMap();
+        java.util.Map<String, String[]> parMap = request.getParameterMap();
 
-        java.util.Collection<String[]> parVal=parMap.values();
+        java.util.Collection<String[]> parVal = parMap.values();
 
         //int numberofPar=parVal.size();
 
-        int i=1;
-        java.util.Iterator<String[]> iter =  parVal.iterator();
+        int i = 1;
+        java.util.Iterator<String[]> iter = parVal.iterator();
 
         String[] nextGroup;
 
         int nextPar;
-        int sumPar=0;
+        int sumPar = 0;
         int groupSize;
-        while (iter.hasNext()){
-            nextGroup=iter.next();
-            groupSize=nextGroup.length;
-            for (i=0; i<groupSize; i++) {
+        while (iter.hasNext()) {
+            nextGroup = iter.next();
+            groupSize = nextGroup.length;
+            for (i = 0; i < groupSize; i++) {
                 nextPar = Integer.parseInt(nextGroup[i]);
                 sumPar = sumPar + nextPar;
             }
